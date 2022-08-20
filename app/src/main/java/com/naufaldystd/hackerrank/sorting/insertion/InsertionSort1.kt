f4e0a3271b6d48f90021a9f9955ef75352ca2975
blitzkz23 <blitzaldykazu@gmail.com>
2022-08-21 04:53:03 +0700

    Add description notes of insertion sort problem

--- app/src/main/java/com/naufaldystd/hackerrank/sorting/insertion/InsertionSort1.kt
+++ app/src/main/java/com/naufaldystd/hackerrank/sorting/insertion/InsertionSort1.kt
@@ -2,7 +2,13 @@ package com.naufaldystd.hackerrank.sorting.insertion
 
 import com.naufaldystd.hackerrank.sorting.insertion.InsertionSort1.insertionSort1
 
+
 object InsertionSort1 {
+	val sebuahNotes = """
+		This problem is a simple version of insertion notes where, given an array and one unsorted number in the rightmost side, I need to sort it by comparing
+		the unsorted one to the one on left side one by one.  If the current index on the loop is bigger than the unsorted one, switch that one to the right side
+		an so on, until the array is sorted.
+	""".trimIndent()
 
 	fun insertionSort1(n: Int, arr: Array<Int>){
 		//	Store last index array
